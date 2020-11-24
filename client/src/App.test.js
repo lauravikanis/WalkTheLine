62475751fd271eca7398dd2f1ef0a2d9837658d4
lauravikanis <lauravikanis@yahoo.de>
2020-11-24 18:35:23 +0100

    delete test for app

--- client/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
-
-test("renders storybook react link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/To Storybook/i);
-  expect(linkElement).toBeInTheDocument();
-});
