d2b537b72f5d76dcad50d6af4d867c52352b79e0
lauravikanis <lauravikanis@yahoo.de>
2020-12-01 16:01:33 +0100

    add styling to BackButton

--- client/src/components/Button/BackButton.js
+++ client/src/components/Button/BackButton.js
@@ -3,9 +3,14 @@ import Backarrow from "../../assets/back.svg";
 import { useHistory } from "react-router-dom";
 
 const Back = styled.button`
-  height: 55px;
+  /* height: 55px; */
   padding: 0 2rem;
   border: none;
+
+  img {
+    height: 55px;
+    padding-bottom: 0;
+  }
 `;
 
 const BackButton = () => {
