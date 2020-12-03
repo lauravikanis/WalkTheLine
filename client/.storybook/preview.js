c6fb40d974329299259f2c8cd2e009885d50b302
lauravikanis <lauravikanis@yahoo.de>
2020-12-03 16:57:12 +0100

    add routing to storybook

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,12 +1,14 @@
+// import { Router } from "express";
 import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import GlobalStyle from "../src/GlobalStyles";
 
 export const decorators = [
   (Story) => (
-    <>
+    <MemoryRouter>
       <GlobalStyle />
       <Story />
-    </>
+    </MemoryRouter>
   ),
 ];
 export const parametesr = {
