4ac848d860259bb5e808e49a8f52f2db311294d6
lauravikanis <lauravikanis@yahoo.de>
2020-12-17 11:19:36 +0100

    add regEx to search filter - to fix casesnsetivity

--- client/src/components/SearchBar/Searchbar.js
+++ client/src/components/SearchBar/Searchbar.js
@@ -1,10 +1,43 @@
-import styled from "styled-components";
+import styled from "styled-components/macro";
+import PropTypes from "prop-types";
 
 const Form = styled.form`
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  input {
+    display: flex;
+    background-color: var(--background);
+    border: none;
+    color: var(--text-color);
+    text-align: center;
+    margin: 0 auto;
+    border-bottom: 3px solid #0f4e8f;
+    width: 100%;
+    padding-bottom: 8px;
+  }
 `;
 
-export default Form;
+const Searchbar = ({ value, onSubmit, onChange, name }) => {
+  return (
+    <Form onSubmit={onSubmit}>
+      <input
+        value={value}
+        onChange={onChange}
+        type="text"
+        placeholder="🔍  Search tags"
+        name={name}
+      />
+    </Form>
+  );
+};
+export default Searchbar;
+
+Searchbar.propTypes = {
+  onChange: PropTypes.func,
+  onSubmit: PropTypes.func,
+  value: PropTypes.string,
+  name: PropTypes.string,
+};
--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -8,9 +8,10 @@ import PageHeadline from "../components/Header/PageHeadline";
 import Input from "../components/Input/Input";
 import LocationList from "../components/Standorte/LocationList";
 
-import Radio from "@material-ui/core/Radio";
-import RadioGroup from "@material-ui/core/RadioGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import Radio from "@material-ui/core/Radio";
+// import RadioGroup from "@material-ui/core/RadioGroup";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Link } from "react-router-dom";
 
 const SearchDiv = styled.div`
   display: flex;
@@ -24,22 +25,22 @@ const SearchDiv = styled.div`
   }
 `;
 
-const RadioForm = styled(RadioGroup)`
-  display: flex;
-  justify-content: center;
+// const RadioForm = styled(RadioGroup)`
+//   display: flex;
+//   justify-content: center;
 
-  span {
-    color: var(--primary-color);
-    font-family: var(--titleFont);
-    font-weight: bold;
-    text-transform: uppercase;
-  }
-`;
+//   span {
+//     color: var(--primary-color);
+//     font-family: var(--titleFont);
+//     font-weight: bold;
+//     text-transform: uppercase;
+//   }
+// `;
 
 const Search = () => {
   const [results, setResults] = useState([]);
   const [searchFilter, setSearchFilter] = useState("");
-  const [type, setType] = useState("");
+  // const [type, setType] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -49,9 +50,9 @@ const Search = () => {
     fetchData();
   }, []);
 
-  const handleChange = (event) => {
-    setType(event.target.value);
-  };
+  // const handleChange = (event) => {
+  //   setType(event.target.value);
+  // };
 
   return (
     <SearchDiv>
@@ -63,7 +64,7 @@ const Search = () => {
         value={searchFilter}
         onChange={(event) => setSearchFilter(event.target.value)}
       />
-      <RadioForm
+      {/* <RadioForm
         row
         aria-label="type"
         name="type1"
@@ -88,15 +89,20 @@ const Search = () => {
           control={<Radio />}
           label="POI"
         />
-      </RadioForm>
+      </RadioForm> */}
       <LocationList>
         {results
 
-          .filter((results) => results.type.includes(type))
-          .filter((results) => results.name.includes(searchFilter))
-          // .filter((results) => results.type.includes(shopFilter))
+          // .filter((results) => results.type.includes(type))
+          // .filter((results) => results.name.includes(searchFilter))
+          .filter((results) => new RegExp(searchFilter, "i").test(results.name))
           .map((filterResult) => (
-            <li key={filterResult.name}> {filterResult.name} </li>
+            <li key={filterResult.name}>
+              <Link to={`/location/${filterResult.name}`}>
+                {" "}
+                {filterResult.name}{" "}
+              </Link>
+            </li>
           ))}
       </LocationList>
     </SearchDiv>
