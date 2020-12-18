6759fb078fe83bd6fadb65ef3b4d2f16136de7b3
lauravikanis <lauravikanis@yahoo.de>
2020-12-18 12:30:08 +0100

    add imagecard with mapped images

--- client/src/pages/Location.js
+++ client/src/pages/Location.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components/macro";
 import PageHeadline from "../components/Header/PageHeadline";
 
-import { Link, useParams } from "react-router-dom";
+import { /* Link, */ useParams } from "react-router-dom";
 import { useQuery } from "react-query";
 import { getLocationByName } from "../api/locations";
 import DetailCard from "../components/Card/LocationDetails";
@@ -11,7 +11,7 @@ import FavoriteButton from "../components/Button/FavoriteButton";
 import LeafletMap from "../components/Map/LeafletMap";
 import Header from "../components/Header/Header";
 import ImageCard from "../components/Card/ImageCard";
-import placeholder from "../assets/placeholder.jpeg";
+// import placeholder from "../assets/placeholder.jpeg";
 
 const LocationDiv = styled.div`
   display: flex;
@@ -40,6 +40,9 @@ const Location = () => {
   if (error) {
     return `Ein Fehler ist aufgetreten: ${error.message}`;
   }
+
+  console.log(locationByName.pic);
+
   return (
     <LocationDiv>
       <Header />
@@ -67,20 +70,14 @@ const Location = () => {
               </p>
             )}
           </DetailCard>
-          <ImageCard>
-            <Link to="/pictures">
-              <img
-                src="https://upload.wikimedia.org/wikipedia/commons/6/68/Empfangsgeb%C3%A4ude_des_Bahnhofs_K%C3%B6ln-West-5921.jpg"
-                alt="placeholder"
-              />
-            </Link>
-            <Link to="/pictures">
-              <img src={placeholder} alt="placeholder" />
-            </Link>
-            <Link to="/pictures">
-              <img src={placeholder} alt="placeholder" />
-            </Link>
-          </ImageCard>
+
+          {locationByName.pic && (
+            <ImageCard>
+              {locationByName.pic.map((name) => (
+                <img key={name.name} src={name.link} alt={name.description} />
+              ))}
+            </ImageCard>
+          )}
           <LeafletMap
             zoomdistance="15"
             mapCenter={locationByName.position}
