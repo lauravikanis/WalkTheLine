d41107266f34947997026ccc8edab5b865024019
lauravikanis <lauravikanis@yahoo.de>
2020-12-21 16:39:58 +0100

    update dependencies

--- client/src/components/Locations/LocationList.stories.js
+++ client/src/components/Locations/LocationList.stories.js
@@ -2,13 +2,13 @@ import React from "react";
 import LocationList from "./LocationList";
 
 export default {
-  title: "Components/Standorte",
+  title: "Components/LocationLists",
   component: LocationList,
 };
 
 const Template = (args) => <LocationList {...args} />;
 
-export const StandortExample = Template.bind({});
-StandortExample.args = {
+export const locationList = Template.bind({});
+locationList.args = {
   children: <li>1. Standort</li>,
 };
