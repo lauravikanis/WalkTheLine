c98901a19b01716392c8ce3432e5d44376a9bd61
lauravikanis <lauravikanis@yahoo.de>
2020-12-30 15:49:42 +0100

    add custom maps with mapbox

--- client/src/components/Map/LeafletMapTour.js
+++ client/src/components/Map/LeafletMapTour.js
@@ -2,6 +2,7 @@ import React from "react";
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
 import styled from "styled-components";
 import "leaflet/dist/leaflet.css";
+import "mapbox-gl/dist/mapbox-gl.css";
 
 import PropTypes from "prop-types";
 import { useLocation } from "react-router-dom";
@@ -11,6 +12,9 @@ import { getTourDetails } from "../../api/locations";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
 import iconShadow from "leaflet/dist/images/marker-shadow.png";
+require("dotenv").config();
+
+const Mapboxtoken = process.env.REACT_APP_MAPBOX_TOKEN;
 
 const Map = styled(MapContainer)`
   height: 40vh;
@@ -45,8 +49,9 @@ const LeafletMapTour = () => {
         </Marker>
       ))}
       <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
+        '
+        url={`https://api.mapbox.com/styles/v1/lauravikanis/ckjbesrwvj9681at4kry1zw4s/tiles/256/{z}/{x}/{y}@2x?access_token=${Mapboxtoken}`}
       />
     </Map>
   );
