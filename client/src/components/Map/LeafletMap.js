83ff4a562bfd6f4602fcc3b9b32fe59c5062ef91
lauravikanis <lauravikanis@yahoo.de>
2021-01-28 18:37:15 +0100

    add darkmode for map

--- client/src/components/Map/LeafletMap.js
+++ client/src/components/Map/LeafletMap.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import "leaflet/dist/leaflet.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -25,6 +25,8 @@ const LeafletMap = ({
   markerPosition,
   locationName,
 }) => {
+  const theme = useTheme().theme;
+
   const { location: currentLocation, error: currentError } = useCurrentLocation(
     geolocationOptions
   );
@@ -44,11 +46,19 @@ const LeafletMap = ({
         <LocationMarker location={currentLocation} error={currentError} />
       )}
 
-      <TileLayer
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
+      {theme === "light" ? (
+        <TileLayer
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
         '
-        url={`https://api.mapbox.com/styles/v1/lauravikanis/ckjbesrwvj9681at4kry1zw4s/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-      />
+          url={`https://api.mapbox.com/styles/v1/lauravikanis/ckjbesrwvj9681at4kry1zw4s/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      ) : (
+        <TileLayer
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
+        '
+          url={`https://api.mapbox.com/styles/v1/lauravikanis/ckkh41jns0pi418o2tvm7vkz1/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      )}
     </Map>
   );
 };
--- client/src/components/Map/LeafletMapTour.js
+++ client/src/components/Map/LeafletMapTour.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
-import styled from "styled-components";
+import styled, { useTheme } from "styled-components";
 import "leaflet/dist/leaflet.css";
 import "mapbox-gl/dist/mapbox-gl.css";
 
@@ -39,6 +39,8 @@ const PopupContainer = styled.div`
 
 const LeafletMapTour = () => {
   let location = useLocation();
+  const theme = useTheme().theme;
+
   const { location: currentLocation, error: currentError } = useCurrentLocation(
     geolocationOptions
   );
@@ -79,12 +81,19 @@ const LeafletMapTour = () => {
       {currentLocation && (
         <LocationMarker location={currentLocation} error={currentError} />
       )}
-
-      <TileLayer
-        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
+      {theme === "light" ? (
+        <TileLayer
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
         '
-        url={`https://api.mapbox.com/styles/v1/lauravikanis/ckjbesrwvj9681at4kry1zw4s/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
-      />
+          url={`https://api.mapbox.com/styles/v1/lauravikanis/ckjbesrwvj9681at4kry1zw4s/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      ) : (
+        <TileLayer
+          attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>
+        '
+          url={`https://api.mapbox.com/styles/v1/lauravikanis/ckkh41jns0pi418o2tvm7vkz1/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
+        />
+      )}
     </Map>
   );
 };
