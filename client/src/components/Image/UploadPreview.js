6777d3f0177c5cc168452a5559f05cffd2e1245d
lauravikanis <lauravikanis@yahoo.de>
2021-02-01 15:17:06 +0100

    build Uploadpage

---
+++ client/src/components/Image/UploadPreview.js
@@ -0,0 +1,40 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components/macro";
+
+const Display = styled.div`
+  background: var(--image-container);
+  height: 50vw;
+  width: 50vw;
+  margin: 2rem auto 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  @media (min-width: 800px) {
+    height: 50vh;
+    width: 50vh;
+  }
+  @media (min-width: 1000px) and (orientation: landscape) {
+    height: 50vh;
+    width: 50vh;
+  }
+`;
+const PreviewImage = styled.img`
+  max-height: 100%;
+  max-width: 100%;
+`;
+
+const UploadPreview = ({ src, alt }) => {
+  return (
+    <Display>
+      <PreviewImage src={src} alt={alt} />
+    </Display>
+  );
+};
+
+export default UploadPreview;
+
+UploadPreview.propTypes = {
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired,
+};
--- client/src/pages/PictureUpload.js
+++ client/src/pages/PictureUpload.js
@@ -1,11 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components/macro";
 
-import placeholder from "../assets/search.svg";
-import Button from "../components/Button/Button";
+// import Button from "../components/Button/Button";
+import UploadPreview from "../components/Image/UploadPreview";
 import Input from "../components/Input/Input";
 
-import { PageHeadline, PictureContainer } from "../imports";
+import { PageHeadline } from "../imports";
 
 const UploadDiv = styled.div`
   display: flex;
@@ -15,18 +16,106 @@ const UploadDiv = styled.div`
   text-align: center;
 `;
 
+const UploadButton = styled.button`
+  background: linear-gradient(160deg, var(--active), var(--active-gradient));
+`;
+
+const ButtonWrapper = styled.div`
+  width: fit-content;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const ImageInput = styled.input`
+  font-size: 1.5rem;
+  border-radius: 50%;
+  width: 100%;
+  position: absolute;
+  z-index: 1;
+`;
+const InputButton = styled.button`
+  color: var(--light);
+  background: var(--active);
+  position: relative;
+  z-index: 2;
+  pointer-events: none;
+`;
+
 const PictureUpload = () => {
+  const [uploadInput, setUploadInput] = useState("");
+  const [previewSrc, setPreviewSrc] = useState("");
+  const history = useHistory();
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    console.log("file", file);
+    previewFile(file);
+    setUploadInput(event.target.value);
+  };
+
+  const previewFile = (file) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setPreviewSrc(reader.result);
+      console.log(reader.result);
+    };
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!previewSrc) {
+      console.log("No preview file");
+    }
+    await uploadImage(previewSrc);
+    history.push("/tagging");
+  };
+
+  const uploadImage = async (base64EncodedImage) => {
+    try {
+      await fetch("/api/upload", {
+        method: "POST",
+        body: JSON.stringify({ image: base64EncodedImage, userName: "sven" }),
+        headers: { "Content-Type": "application/json" },
+      });
+      setUploadInput("");
+      setPreviewSrc("");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <UploadDiv>
       <PageHeadline>Upload</PageHeadline>
-      <PictureContainer>
-        <img src={placeholder} alt="placeholder" />
-      </PictureContainer>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <ButtonWrapper>
+          <InputButton
+            style={{
+              background: previewSrc
+                ? "var(--background)"
+                : "linear-gradient(160deg, var(--active), var(--active-gradient))",
+            }}
+            htmlFor="upload"
+          >
+            Bild auswählen
+          </InputButton>
+          <ImageInput
+            type="file"
+            value={uploadInput}
+            onChange={handleImageChange}
+          />
+        </ButtonWrapper>
         <Input placeholder="Bildname" />
         <Input placeholder="Bildbeschreibung" />
+        {previewSrc && <UploadPreview src={previewSrc} alt="" />}
+        <div>
+          {previewSrc && (
+            <UploadButton type="submit">Bild hochladen</UploadButton>
+          )}
+        </div>
       </form>
-      <Button value="Upload" type="button" onClick="handleClick" />
     </UploadDiv>
   );
 };
