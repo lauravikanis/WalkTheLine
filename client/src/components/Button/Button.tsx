95d94961a8ec8e41c535ffb1e717b4e60d86b6af
lauravikanis <lauravikanis@yahoo.de>
2021-06-28 20:36:05 +0200

    :construction: trying new things

--- client/src/components/Button/Button.tsx
+++ client/src/components/Button/Button.tsx
@@ -1,71 +1,12 @@
 import React from "react";
-import styled, { useTheme } from "styled-components/macro";
-import { ReactComponent as Toggler } from "../../assets/sun.svg";
-import { ReactComponent as TogglerDark } from "../../assets/night-mode.svg";
-import { ReactComponent as FavOn } from "../../assets/favorite_on.svg";
-import { ReactComponent as FavOff } from "../../assets/favorite_off.svg";
-import Backarrow from "../../assets/back.svg";
 import { useHistory } from "react-router";
-
-const IconButton = styled.button`
-  margin: 0;
-  padding: 0;
-  color: grey;
-  background: none;
-  transition: 0.5s;
-  border: none;
-
-  svg {
-    width: 100%;
-    max-width: 70px;
-    height: 100%;
-    padding: 0;
-    margin: 0;
-    margin-right: 1rem;
-  }
-`;
-const FavoriteIcon: any = styled.button`
-  padding: 0;
-  background: none;
-  width: 24px;
-  height: 24px;
-  margin-left: 1rem;
-  padding-top: 1px;
-  border: none;
-`;
-const UploadButton = styled.button`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: var(--primary-color);
-  border-radius: 15px;
-  height: 50px;
-  margin: 15px auto;
-  color: white;
-  font-size: 1rem;
-  text-align: center;
-  font-weight: bold;
-  text-transform: uppercase;
-  padding-left: 1rem;
-  padding-right: 1rem;
-  border: none;
-
-  :active {
-    background-color: white;
-    color: var(--primary-color);
-  }
-`;
-
-const Back = styled.button`
-  padding: 0 2rem;
-  background: none;
-  border: none;
-
-  img {
-    height: 55px;
-    padding-bottom: 0;
-  }
-`;
+import { useTheme } from "styled-components/macro";
+import Backarrow from "../../assets/back.svg";
+import { ReactComponent as FavOff } from "../../assets/favorite_off.svg";
+import { ReactComponent as FavOn } from "../../assets/favorite_on.svg";
+import { ReactComponent as TogglerDark } from "../../assets/night-mode.svg";
+import { ReactComponent as Toggler } from "../../assets/sun.svg";
+import "./button.css";
 
 const Button: React.ElementType = ({
   onClick,
@@ -81,29 +22,33 @@ const Button: React.ElementType = ({
 
   const rendeFavoriteButton = () => {
     return (
-      <FavoriteIcon onClick={onClick}>
+      <button onClick={onClick} className="button_favorite">
         {isFavorite ? <FavOn /> : <FavOff />}
-      </FavoriteIcon>
+      </button>
     );
   };
 
   const renderThemeToggle = () => {
     return (
-      <IconButton onClick={onClick}>
+      <button onClick={onClick} className="button_theme">
         {themepicker === "light" ? <Toggler /> : <TogglerDark />}
-      </IconButton>
+      </button>
     );
   };
 
   const renderUploadButton = () => {
-    return <UploadButton type={type}>Daten hochladen</UploadButton>;
+    return (
+      <button type={type} className="button_upload">
+        Daten hochladen
+      </button>
+    );
   };
 
   const renderBackButton = () => {
     return (
-      <Back onClick={() => history.goBack()}>
+      <button onClick={() => history.goBack()} className="button_back">
         <img src={Backarrow} alt="Go back" />
-      </Back>
+      </button>
     );
   };
 
--- client/src/components/Card/Card.tsx
+++ client/src/components/Card/Card.tsx
@@ -1,45 +1,15 @@
-import styled from "styled-components/macro";
-
-const Cardwrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* background: var(--primary-color); */
-  background: ${(props) => props.theme.cardBackground};
-
-  border-radius: 15px;
-  width: 100%;
-  height: 100px;
-  margin: 15px auto;
-  padding: 0 2.5rem;
-  color: var(--secondary-color);
-
-  h2 {
-    color: var(--secondary-color);
-    text-align: left;
-  }
-  img {
-    width: 50px;
-    height: 50px;
-    margin-left: 30px;
-  }
-
-  p {
-    text-align: center;
-    display: inline-block;
-  }
-`;
+import "./card.css";
 
 const Card: any = ({ name, src, alt }: any) => {
   return (
-    <Cardwrapper>
+    <div className="card_basic">
       {name && (
         <>
           <h2>{name}</h2>
           <img src={src} alt={alt} />
         </>
       )}
-    </Cardwrapper>
+    </div>
   );
 };
 
